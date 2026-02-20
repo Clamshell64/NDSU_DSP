188fe3d0747ba47fa8ae5d293695e7274bd6e759
Clamshell64 <dylanmalsed@gmail.com>
2026-02-19 20:44:11 -0600

    hw3 update

--- hw3_signal_gen/main.c
+++ hw3_signal_gen/main.c
@@ -1,6 +1,6 @@
 /**************************************************************************/
 //Name:  main.c	
-//Purpose:  Main program for Spectral Inverter HW2 
+//Purpose:  Fourier series approximation of signal generation using timer interrupts and DAC output								// 
 //Author: Dylan Malsed																									
 //Target:  Freescale K22f						
 /**************************************************************************/
@@ -20,43 +20,26 @@ float w0 = 2.0f * PI / 40.0f;
 float c1 = 0.5f;
 float c2 = 1.2764f;
 float c3 = 5000.0f;
+int fourier_order = 10;
 
 float ck_real[11] = {0.5750, -0.0329, -0.2482, 0.0650, -0.0099, -0.0399, 0.0031, 0.0262, -0.0527, 0.0333, -0.004}; // real coefficients
-float ck_imag[11] = {0.0, -0.1354, -0.0874, 0.1322, 0.0853, 0.008, 0.0473, 0.0566, 0.03, 0.0027, -0.0159}; // imaginary coefficients
-
-
-/* small software delay utility function */
-static void delay(volatile uint32_t d){ while(d--) __NOP(); }
+float ck_imag[11] = {0.0, -0.1354, -0.0874, 0.1322, -0.0853, 0.008, 0.0473, -0.0566, 0.03, 0.0027, -0.0159}; // imaginary coefficients
 
 
 void LED_Init(void){ // initialize RGB LED pins
 	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTD_MASK;
 
 	/* set LED pins to GPIO */
-	PORTA->PCR[1] = PORT_PCR_MUX(1);  /* PTA1 - Red */
-	PORTA->PCR[2] = PORT_PCR_MUX(1);  /* PTA2 - Green*/
-	PORTD->PCR[5] = PORT_PCR_MUX(1);  /* PTD5 - Blue*/
 	PORTA->PCR[5] = PORT_PCR_MUX(1);  /* PTA5 - general purpose - I'm going to connect an LED to it*/
 
 	/* configure as outputs  (PDDR - pin direction: 1 = output)*/
-	PTA->PDDR |= (1u<<1) | (1u<<2) | (1u << 5);;
-	PTD->PDDR |= (1u<<5);
+	PTA->PDDR |= (1u << 5);;
 
 	/* default to OFF ---- PSOR = set (off), PCOR = clear (on) ---- onboard RGB LED is ACTIVE LOW */
-	PTA->PSOR = (1u<<1) | (1u<<2) | (1u << 5);
-	PTD->PSOR = (1u<<5);
+	PTA->PSOR = (1u << 5);
 }
 
 
-void LED_cycle(void){
-	/* PTA1 */
-	PTA->PCOR = (1u<<1); delay(4000000); PTA->PSOR = (1u<<1); delay(2000000);
-	/* PTA2 */
-	PTA->PCOR = (1u<<2); delay(4000000); PTA->PSOR = (1u<<2); delay(2000000);
-	/* PTD5 */
-	PTD->PCOR = (1u<<5); delay(4000000); PTD->PSOR = (1u<<5); delay(2000000);
-	delay(100000);
-}
 
 
 int yk(int order, int time){
@@ -64,10 +47,12 @@ int yk(int order, int time){
 
 	float t = (float)time / 10000.0f; // convert time int counter to a ms scale - 10kHz sample rate
 
+	// calculate Fourier series approximation for this time step
 	for (int k=1; k <= order; k++){
 		y += 2.0f * ((ck_real[k] * cosf(w0 * k *c3 * t)) - (ck_imag[k] * sinf(w0 * k * c3 * t)));
 	}
 
+	// scale and shift to maximize amplitude and fit in 0-3V range with a 10% buffer
 	y *= c2;
 	y += c1;
 
@@ -87,9 +72,8 @@ void PIT0_IRQHandler(void){	//This function is called when the timer interrupt e
 	PTA->PCOR = (1u<<5); // LED on to indicate we're in the ISR
 
 	/* output to dac */ 
-	DAC_SetRaw(yk(5, time));
-	time = (time + 1) % 80; // period = T/c3 = 40ms, so with 10kHz sample rate, we have 400 samples per period
-	//delay(1000);
+	DAC_SetRaw(yk(fourier_order, time));
+	time = (time + 1) % 80; // period = T/c3 = 8ms, so with 10kHz sample rate, we have 80 samples per period
 	PTA->PSOR = (1u<<5); // LED off to indicate we're leaving the ISR
 }
 
@@ -98,21 +82,11 @@ void PIT0_IRQHandler(void){	//This function is called when the timer interrupt e
 
 int main(void){
 	MCG_Clock120_Init();
-	ADC_Init();
-	ADC_Calibrate();
 	DAC_Init();
 	TimerInt_Init();
 	LED_Init();
 	
 	while(1){
 		
-		//LED_cycle();
-		// for (float v = 0.0f; v <= 2.0f * PI; v += 0.01f){ 
-		// 	//4095 max
-		// 	float s = sinf(v);
-		// 	uint16_t dac = (uint16_t)((s * 0.5f + 0.5f) * 4095.0f); // scale to 0..4095
-		// 	DAC_SetRaw(dac);
-		// 	delay(10000u); // ~pause so you can observe each voltage
-		// }
 	}
 }
